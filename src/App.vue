1f5a1b97623ca4ab10ea3753a122233d934cdee5
Unknown <enoragon@gmail.com>
2018-01-06 01:51:51 -0600

    Beginning to add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+@import '~/vuetify/src/stylus/main.styl'
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-alert color="success" icon="check_circle" value="true">
+        This is a success alert.
+        </v-alert>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
